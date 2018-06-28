ff552e9193e7cf5cd04c73cf84476660b5d6ada9
Sydney Mercier <sydney.mercier91@gmail.com>
2018-06-27 22:28:36 -0400

    user dao and implementation

--- src/main/java/com/revature/daos/UserDao.java
+++ src/main/java/com/revature/daos/UserDao.java
@@ -8,8 +8,8 @@ public interface UserDao {
 	
 	public List<User> getUsers();
 	public User getUserById(int id);
-	public void createUser(String firstname, String lastname, String email, String phone);
-	public void updateUser(int id, String firstname, String lastname, String email, String phone);
+	public void createUser(User user);
+	public void updateUser(User user);
 	public int deleteUserById(int id);
 
 }
